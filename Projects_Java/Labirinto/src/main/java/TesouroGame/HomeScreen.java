9bbd2c940d4620113d0b8770d67ac51b88674abd
Rafael Lobato <Rafaelcristolobato@gmail.com>
2025-01-22 13:41:51 -0300

    config inicial da homeScreen

---
+++ Projects_Java/Labirinto/src/main/java/TesouroGame/HomeScreen.java
@@ -0,0 +1,14 @@
+package TesouroGame;
+
+import javax.swing.*;
+
+public class HomeScreen extends JFrame {
+    public HomeScreen(){
+
+        setVisible(true);
+        setSize(800, 500);
+        setTitle("TresureGame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+}
---
+++ Projects_Java/Labirinto/src/main/java/TesouroGame/Main.java
@@ -0,0 +1,7 @@
+package TesouroGame;
+
+public class Main {
+    public static void main(String[] args){
+        new HomeScreen();
+    }
+}
